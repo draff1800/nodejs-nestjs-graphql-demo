83178be69bd1a1f9deb8501e488e21a645e59cf9
draff1800 <draff1800@gmail.com>
2025-06-06 00:59:50 -0400

    Compled core nestjs graphql implementation

--- src/cats/cats.resolver.ts
+++ src/cats/cats.resolver.ts
@@ -1,4 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { CatsService } from './cats.service';
+import { Cat } from './models/cat.model';
+import { CreateCatInput } from './dto/create-cat.input';
 
-@Resolver()
-export class CatsResolver {}
+@Resolver(() => Cat)
+export class CatsResolver {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Query(() => [Cat], { name: 'cats' })
+  findAll() {
+    return this.catsService.findAll();
+  }
+
+  @Mutation(() => Cat)
+  createCat(@Args('createCatInput') createCatInput: CreateCatInput) {
+    return this.catsService.create(createCatInput);
+  }
+}
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Cat } from './models/cat.model';
+import { CreateCatInput } from './dto/create-cat.input';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  private cats: Cat[] = [];
+
+  create(createCatInput: CreateCatInput): Cat {
+    const cat: Cat = { id: Date.now().toString(), ...createCatInput };
+    this.cats.push(cat);
+    return cat;
+  }
+
+  findAll(): Cat[] {
+    return this.cats;
+  }
+}
